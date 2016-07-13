5da0422370d69edfb3c43621ea2c13b50614786d
mkazants <mkazants1@gmail.com>
2016-07-12 23:10:44 -0700

    Add files via upload

--- subquery.sql
+++ subquery.sql
@@ -1,3 +1,4 @@
+/* Select the order id,shipped date, and fright cost for the order with the highest freight cost shipped before 1997 (rounded to the nearest cent) */
 select orderid,shippeddate,freight
 from orders
 where freight = (select max(freight) from orders where shippeddate<'1997-01-01')
