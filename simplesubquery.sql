d78926e4c5c6d0bd9237c1ba484abdee6da8bf10
mkazants <mkazants1@gmail.com>
2016-07-14 15:39:00 -0700

    Delete simplesubquery.sql

--- simplesubquery.sql
+++
@@ -1,4 +0,0 @@
-/* Select the order id,shipped date, and fright cost for the order with the highest freight cost shipped before 1997 */
-select orderid,shippeddate,freight
-from orders
-where freight = (select max(freight) from orders where shippeddate<'1997-01-01')
